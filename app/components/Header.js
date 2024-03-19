692195b99af31a3ac779f7f4bbdca40b45377895
Kale <kaleckh@gmail.com>
2024-03-19 15:39:10 -0600

    triggering changes

--- app/components/Header.js
+++ app/components/Header.js
@@ -18,7 +18,7 @@ export default function Header() {
         <div className={styles.logoSpaceContainer}>
           <div className={styles.logoSpace}>
             <Link href={'/'}>
-              <div className={styles.logoContainers}>
+              {/* <div className={styles.logoContainers}>
                 <Image
                   src={Logo}
                   alt="Picture of the author"
@@ -28,7 +28,7 @@ export default function Header() {
                     height: 'auto',
                   }}
                 />
-              </div>
+              </div> */}
             </Link>
             <div className={styles.pieceContainer}>
               <Link href="/">
